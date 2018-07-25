b2183b3772c07073d867e83bb833261efdfdc035
Aurpa Anindita <464438anindita@gmail.com>
2018-07-25 04:53:44 +0000

    Classwork8

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
- var http = require('http');
+var http = require('http');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+//for local
+//var db, uri = "mongodb://localhost:27017";
+//for c9
+var db, uri = "mongodb://"+process.env.IP+":27017";
+mongo.MongoClient.connect(uri, 
+    {useNewUrlParser:true},
+    function(err, client){
+if (err){
+    console.log('Could not connect to mongoDB')
+}else{
+    db = client.db('node-cw8');
+}
+})
+
+var save = function(form_data){
+    db.createCollection('users', function(err, collection){});
+    var collection = db.collection('users');
+    collection.save(form_data);
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extender:true}));
 var server = http.Server(app);
@@ -19,6 +39,8 @@ app.get('/system/form', function(req,res){
 
 app.post('/submit_user', function(req,res){
    console.log(req.body);
+   save(req.body);
+   res.status(200);
 });
 
   server.listen(process.env.PORT, process.env.IP, function(){
