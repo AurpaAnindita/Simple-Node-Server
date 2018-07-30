98224c47492fe50f5b9992d88be29c94d6c2cc9d
Aurpa Anindita <464438anindita@gmail.com>
2018-07-30 04:36:39 +0000

    Classwork 9

--- server.js
+++ server.js
@@ -2,11 +2,34 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb');
+var mongoose = require('mongoose');
+//mongoose.connect("mongodb://"+process.env.IP+":27017/node-cw8");
+mongoose.connect("mongodb://localhost:27017/node-cw8");
+
+mongoose.connection.on('error', function(){
+    console.log('Could not connect to mongodb');
+})
+
+// define user schema with mongoose
+var userSchema = new mongoose.Schema({
+ // name: String,
+  //email: String
+  name:{
+      type: String,
+      required: "Name is required"
+  },
+  email: String
+});
+var User = mongoose.model('User', userSchema);
+
+
+/*var mongo = require('mongodb');
 //for local
 //var db, uri = "mongodb://localhost:27017";
 //for c9
 var db, uri = "mongodb://"+process.env.IP+":27017";
+
+
 mongo.MongoClient.connect(uri, 
     {useNewUrlParser:true},
     function(err, client){
@@ -17,8 +40,11 @@ if (err){
 }
 })
 
+*/
+
 var save = function(form_data){
     db.createCollection('users', function(err, collection){});
+    form_data.age = 12;
     var collection = db.collection('users');
     collection.save(form_data);
 }
@@ -39,8 +65,15 @@ app.get('/system/form', function(req,res){
 
 app.post('/submit_user', function(req,res){
    console.log(req.body);
-   save(req.body);
-   res.status(200);
+   var new_user = new User(req.body);
+   new_user.save(function(err, data){
+       if(err){
+           return res.status(400).json({message:"Could not save user"})
+       }
+       res.status(200).json(data);
+   })
+  // save(req.body);
+  // res.status(200);
 });
 
   server.listen(process.env.PORT, process.env.IP, function(){
